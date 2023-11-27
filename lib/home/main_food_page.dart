0afa7d77ad52f863120f8f5aa7eae3fe095c73e9
Shalini yadav <shaliniyadav0203@gmail.com>
2023-11-27 11:00:32 +0530

    Food page updated

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,71 @@
+
+import 'package:ecommerce/home/food_page_body.dart';
+import 'package:ecommerce/utils/colors.dart';
+import 'package:ecommerce/utils/dimensions.dart';
+import 'package:ecommerce/widgets/big_text.dart';
+import 'package:ecommerce/widgets/small_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+                margin: EdgeInsets.only(top:Dimensions.height45,bottom:Dimensions.height15),
+                padding: EdgeInsets.only(left:Dimensions.width20,right:Dimensions.width20),
+                  child:Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              BigText(text: "Uttar Pradesh",color: AppColors.mainColor,),
+                              Row(
+                                children: [
+                                  SmallText(text: "Ghaziabad",color: Colors.black54,),
+                                  Icon(Icons.arrow_drop_down)
+
+                                ],
+                              ),
+                              ],
+                          ),
+
+                          Center(
+                            child: Container(
+                              width: Dimensions.height45,
+                              height: Dimensions.height45,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(Dimensions.radius15),
+                                  color: AppColors.mainColor
+                              ),
+                              child: Icon(Icons.search,color: Colors.white,size: Dimensions.iconSize24,),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+              ),
+
+          Expanded(
+              child:SingleChildScrollView(
+                child: FoodPageBody(),
+              ) ),
+
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/home/main_food_page.dart';
 import 'package:ecommerce/users/authentication/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,66 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Clothes App ',
+      title: 'ecommerce App ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: FutureBuilder(
-          builder: (context, dataSnapShot)
-              {
-                return LoginScreen();
-              },
-      ),
+      home: MainFoodPage()
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
